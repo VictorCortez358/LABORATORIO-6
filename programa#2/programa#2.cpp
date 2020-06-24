5e18a13ecfa65337326620c1954506059b896988
VictorCortez358 <00022120@uca.edu.sv>
2020-06-24 11:42:39 -0600

    first commit

--- programa#2/programa#2.cpp
+++ programa#2/programa#2.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 int main(void)
 {
-    int arreglo[10]={2,3,4,5,5,4,6,1,3,5};
+    int arreglo[10];
     int n,i,cont=0;
 	cout<<"VERIFICAR CUANTAS VECES SE REPITE UN NUMERO ENTERO INGRESADO DENTRO DEL ARREGLO"<<endl<<endl;
-    cout<<"Ingrese un numero entero: ";
-    cin>>n;// se le pide al usuario que ingrese numero entero
 
-		for (i=0;i<10;i++){// se coloca la cantidad de numeros que se encuentra dentro del arreglo.
-		if (n==arreglo[i]){//instruccion que no servira para contabilizar la cantidad de veces que se repite un numero
+		for (i=0;i<10;i++){// se coloca la cantidad de numeros que se encontraran dentro del arreglo.
+		cout<<"Ingrese un numero entero: ";
+        cin>>arreglo[i];// se le pide al usuario que ingrese numero entero
+	    }
+		cout<<"Ingrese el numero que desea saber cuantas veces se repite: ";
+		cin>>n;
+		for (i=0;i<10;i++){
+		if (n==arreglo[i]){//instruccion que nos servira para contabilizar la cantidad de veces que se repite un numero
 			cont++;         
 		}
 	}
-cout<<"El numero "<<n<<" se repite "<<cont<<" veces "<<endl;
+    cout<<"El numero "<<n<<" se repite "<<cont<<" veces "<<endl;
 	cout<<endl;//Se coloca en pantalla la cantidad de veces que se repitio el numero ingresado por el usuario.
     return 0;
 
